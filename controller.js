8a75d618ed6bd6b21fe1639f7d0fead0a72fba3c
agc123 <agc123@yeah.net>
2016-09-01 12:37:06 -0400

    login using angular

---
+++ controller.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module('mainApp',['ngRoute']);
+
+    app.config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'dashboard/dashboard.html',
+            
+            })
+            .when('/login', {
+                templateUrl: 'login/login.view.html',
+            })
+
+            .otherwise({redirectTo: '/login' });
+    });
+
+    
+
+})();
