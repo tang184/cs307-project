7a753d88bf42062afd6c8d398f1472486bb7c42c
agc123 <agc123@yeah.net>
2016-09-03 22:17:57 -0400

    add flash head and confirm password for register

--- login/login.controller.js
+++ login/login.controller.js
@@ -5,12 +5,18 @@
         .module('mainApp')
         .controller('LoginController', LoginController);
 
-    LoginController.$inject = ['$scope', '$location'];
-    	function LoginController($scope, $location) {
+    LoginController.$inject = ['$scope', '$location', 'FlashService'];
+    	function LoginController($scope, $location, FlashService) {
+    		
         	$("#bodyBackground").css('background-image', 'url(../assets/image-resources/blurred-bg/blurred-bg-3.jpg)');
 
         	$scope.register = function() {
         		$location.path('/register');
+        	}
+
+        	$scope.login = function() {
+        		FlashService.Success('Login successful', true);
+        		$location.path('/dashboard');
         	}
     	};
 
--- register/register.controller.js
+++ register/register.controller.js
@@ -9,8 +9,25 @@
         function RegisterController($location, $scope) {
             $("#bodyBackground").css('background-image', 'url(../assets/image-resources/blurred-bg/blurred-bg-2.jpg)');
 
+            $scope.vm = {};
+            $scope.confirmPassword = false;
+
             $scope.back = function() {
                 $location.path('/');
+            }
+
+            $scope.submit = function(vm) {
+
+            }
+
+
+            $scope.checkPassword = function() {
+                if ($scope.vm.user.password == $scope.c_password) {
+                    $scope.confirmPassword = true;
+                } else {
+                    $scope.confirmPassword = false;
+                }
+
             }
         }
 
