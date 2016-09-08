d37ed12c37b685c289e0fc9c3970211887811440
agc123 <agc123@yeah.net>
2016-09-07 20:00:39 -0400

    fb to central

--- controller.js
+++ controller.js
@@ -19,7 +19,7 @@
             })
 
             .state('dashboard', {
-                resolve: {
+                /*resolve: {
                     "check": function($location, $rootScope) {
                         if (!$rootScope.loggedIn) {
                             $scope.$apply(function() { // simple trick otherwise dashboard page will still be loaded.
@@ -27,7 +27,7 @@
                             });
                         }
                     }
-                },
+                },*/
                 parent:'main',
                 controller: 'DashboardController',
                 templateUrl: 'dashboard/dashboard.view.html',
@@ -43,6 +43,30 @@
 
             $urlRouterProvider.otherwise('/login')
     });
+
+
+
+    window.fbAsyncInit = function() {
+              FB.init({
+                appId      : '1793641007572063',
+                cookie     : true,  // enable cookies to allow the server to access
+                xfbml      : true,  // parse social plugins on this page
+                version    : 'v2.5' // use graph api version 2.5
+              });
+              /*FB.getLoginStatus(function(response) {
+                statusChangeCallback(response);
+              });*/
+
+            };
+
+  // Load the SDK asynchronously
+              (function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = "//connect.facebook.net/en_US/sdk.js";
+                fjs.parentNode.insertBefore(js, fjs);
+              }(document, 'script', 'facebook-jssdk'));
 
 
 
--- login/login.controller.js
+++ login/login.controller.js
@@ -21,27 +21,7 @@
                     'into Facebook.';
                 }
             }*/
-            window.fbAsyncInit = function() {
-              FB.init({
-                appId      : '1793641007572063',
-                cookie     : true,  // enable cookies to allow the server to access
-                xfbml      : true,  // parse social plugins on this page
-                version    : 'v2.5' // use graph api version 2.5
-              });
-              /*FB.getLoginStatus(function(response) {
-                statusChangeCallback(response);
-              });*/
-
-            };
-
-  // Load the SDK asynchronously
-              (function(d, s, id) {
-                var js, fjs = d.getElementsByTagName(s)[0];
-                if (d.getElementById(id)) return;
-                js = d.createElement(s); js.id = id;
-                js.src = "//connect.facebook.net/en_US/sdk.js";
-                fjs.parentNode.insertBefore(js, fjs);
-              }(document, 'script', 'facebook-jssdk'));
+            
 
               /*function testAPI() {
                 console.log('Welcome!  Fetching your information.... ');
@@ -70,27 +50,33 @@
             $scope.myFacebookLogin = function() {
                 FB.getLoginStatus(function(response) {
                     if (response.status === 'connected') {
-                        FB.api('/me', function(response1) {
-                            console.log('Successful login for: ' + response1.name);
-                            $scope.$apply( function () { $location.path('/main/dashboard').replace() } );
-                        });
+                        if (!$rootScope.loggedIn) {
+                            $rootScope.loggedIn = true;
+                        }
+                        var myVar = setInterval(FlashService.Success, 2000);
+                        FlashService.Success('Login successful', true);
+                        $scope.$apply( function () { $location.path('/main/dashboard').replace() } );
                     } else if (response.status == 'not_authorized') {
                         FB.login(function(response) {
                             if (response.status === 'connected') {
-                                FB.api('/me', function(response1) {
-                                    console.log('Successful login for: ' + response1.name);
-                                    $scope.$apply( function () { $location.path('/main/dashboard').replace() } );
-                                });
+                                if (!$rootScope.loggedIn) {
+                                    $rootScope.loggedIn = true;
+                                }
+                                var myVar = setInterval(FlashService.Success, 2000);
+                                FlashService.Success('Login successful', true);
+                                $scope.$apply( function () { $location.path('/main/dashboard').replace() } );
                             }
 
                         });
                     } else {
                         FB.login(function(response) {
                             if (response.status === 'connected') {
-                                FB.api('/me', function(response1) {
-                                    console.log('Successful login for: ' + response1.name);
-                                    $scope.$apply( function () { $location.path('/main/dashboard').replace() } );
-                                });
+                                if (!$rootScope.loggedIn) {
+                                    $rootScope.loggedIn = true;
+                                }
+                                var myVar = setInterval(FlashService.Success, 2000);
+                                FlashService.Success('Login successful', true);
+                                $scope.$apply( function () { $location.path('/main/dashboard').replace() } );
                             }
                         });
 
