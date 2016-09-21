41bd51ff3cd29264097c7f007eb96c7fa3be1b7e
agc123 <agc123@yeah.net>
2016-09-20 20:18:13 -0400

    post request

--- register/register.controller.js
+++ register/register.controller.js
@@ -5,8 +5,8 @@
         .module('mainApp')
         .controller('RegisterController', RegisterController);
 
-    RegisterController.$inject = ['$location', '$scope'];
-        function RegisterController($location, $scope) {
+    RegisterController.$inject = ['$location', '$scope', '$http'];
+        function RegisterController($location, $scope, $http) {
             $("#bodyBackground").css('background-image', 'url(assets/image-resources/blurred-bg/blurred-bg-7.jpg)');
 
             $scope.vm = {};
@@ -17,6 +17,32 @@
             }
 
             $scope.submit = function(vm) {
+                var mydata = $.param({
+                    email: vm.user.email,
+                    name: vm.user.userName,
+                    password: vm.user.password
+                });
+                var myconfig = {
+
+                }
+
+                $http({
+                    method:'POST',
+                    headers: { 'Content-Type': 'application/x-www-form-urlencoded', 
+                        'Access-Control-Allow-Origin': true
+                    },
+                    url:'https://yakume.xyz/api/register',
+                    data:mydata, 
+                    config:myconfig
+                })
+                .success(function (data, status, headers, config) {
+                    alert("success.");
+                })
+                .error(function (response, data, status, headers, config) {
+                    console.log(response);
+                });
+
+
 
             }
 
