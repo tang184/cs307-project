b86d93941196a315b0cd55fe1f9ba22d2349c348
Chuyang <s3cy1996@gmail.com>
2016-10-01 19:35:28 -0400

    profile page basic ui

--- userProfile/profile.controller.js
+++ userProfile/profile.controller.js
@@ -5,12 +5,15 @@
         .module('mainApp')
         .controller('ProfileController', ProfileController);
 
-    ProfileController.$inject = ['$scope', '$location', 'FlashService'];
-        function ProfileController($scope, $location, FlashService) {
-            
-            $("#bodyBackground").css('background', 'white');
+        ProfileController.$inject = ['$scope', '$location', 'FlashService'];
+            function ProfileController($scope, $location, FlashService) {
 
-        };
+                $("#bodyBackground").css('background', 'white');
 
+            };
 
+        function ProfileController($scope) {
+            $scope.tab = 1;
+
+        }
 })();
