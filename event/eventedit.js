552800961226a467ed222e855d8aef704fade1d8
unknown <zblcm3@gmail.com>
2016-10-02 04:46:49 -0400

    Some change about event edit page

--- event/eventedit.js
+++ event/eventedit.js
@@ -5,24 +5,17 @@
         .module('mainApp')
         .controller('EventEdit',EventEdit);
 
-        ProfileController.$inject = ['$scope', '$location', 'FlashService'];
-            function ProfileController($scope, $location, FlashService) {
+        EventEdit.$inject = ['$scope', '$location', 'FlashService'];
+            function EventEdit($scope, $location, FlashService) {
 
                 $("#bodyBackground").css('background', 'white');
 
 
         function EventEdit($scope) {
-            $scope.panel1 = true;
-            $scope.panel2 = false;
+            $scope.create = true;
 
-            $scope.toggle1 = function() {
-                $scope.panel1 = true;
-                $scope.panel2 = false;
-            }
-
-            $scope.toggle2 = function() {
-                $scope.panel1 = false;
-                $scope.panel2 = true;
+            $scope.test = function() {
+                alert("test");
             }
         };
 
