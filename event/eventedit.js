1144561f0ecadb8ac3bf692b151f42bb7ded8b55
unknown <zblcm3@gmail.com>
2016-10-02 22:25:35 -0400

    Add date chooser to event edit page

--- event/eventedit.js
+++ event/eventedit.js
@@ -15,8 +15,18 @@
                 $scope.test = function() {
                     console.log("hello");
                 }
+                $scope.showmap = function() {
+			var mapCanvas = document.getElementById("map");
+			var mapOptions = {
+				center: new google.maps.LatLng(51.5, -0.2), 
+				zoom: 10
+			}
+			var map = new google.maps.Map(mapCanvas, mapOptions);
+                }
+                $scope.Cnext = function() {
+                    console.log("next month");
+                }
 
-
-        
+		
         }
 })();
