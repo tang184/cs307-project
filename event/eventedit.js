99234888aabcc844eff38347751b4617ac24eed2
agc123 <agc123@yeah.net>
2016-10-10 23:22:54 -0400

    fix bugs

--- event/eventedit.js
+++ event/eventedit.js
@@ -8,9 +8,7 @@
         EventEdit.$inject = ['$scope', '$location', 'FlashService','$http'];
         function EventEdit($scope, $location, FlashService, $http) {
                 $scope.event={};
-		$scope.test = function() {
-		    new Date().getTime()
-		}
+		
 
                 $("#bodyBackground").css('background', 'white');
 
@@ -55,6 +53,13 @@
                                 if (address) {
                                     $scope.event.lat = address.geometry.location.lat;
                                     $scope.event.lng = address.geometry.location.lng;
+                                    var tmp = address.address_components
+                                    for (var i in tmp) {
+                                        if (tmp[i].types[0] == "postal_code") {
+                                            $scope.event.zip = tmp[i].long_name;
+                                        }
+                                    }
+                                    console.log($scope.event.zip);
                                     $scope.mapurl="https://maps.googleapis.com/maps/api/staticmap?center=" + $scope.event.lat + "," + $scope.event.lng + 
                                     "&zoom=16&size=320x200&&markers=color:red%7Clabel:C%7C" + $scope.event.lat + "," + $scope.event.lng 
                                     + "&key=AIzaSyAFhzO5tGWXiCCtH5y6XW6ycS-1fbC4uYA";
@@ -70,37 +75,28 @@
                 }
 
                 $scope.eventsubmit = function(event) {
-		    var start_date = $('.start_datepicker').val();
-		    var start_time = $('.start_timepicker').val();
-		    var start_stamp = new Date(start_date + " " + start_time).getTime()
+        		    var start_date = $('.start_datepicker').val();
+        		    var start_time = $('.start_timepicker').val();
+        		    var start_stamp = new Date(start_date + " " + start_time).getTime()
 
-		    var end_date = $('.end_datepicker').val();
-		    var end_time = $('.end_timepicker').val();
-		    var end_stamp = new Date(end_date + " " + end_time).getTime()
+        		    var end_date = $('.end_datepicker').val();
+        		    var end_time = $('.end_timepicker').val();
+        		    var end_stamp = new Date(end_date + " " + end_time).getTime()
 
-		    var duration = end_stamp - start_stamp
-
-                    // $scope.event.title = title
-                    // $scope.event.description = description
-                    $scope.event.longitude = 10
-                    $scope.event.latitude = 20
                     $scope.event.timestamp = start_stamp
-                    $scope.event.duration = duration
-                    $scope.event.zipcode = 10000
+                    $scope.event.duration = end_stamp - start_stamp;
 
-		    console.log($scope.event)
+		            console.log($scope.event);
 
-		    $.ajax({
-                        type: "POST",
-                        url: 'https://yakume.xyz/api/addevent',
-                        data: $scope.event,
-                        success: function(response){
-                            console.log(response);
-                        }
-                    });
-                }
-
-                
+        		    $.ajax({
+                                type: "POST",
+                                url: 'https://yakume.xyz/api/addevent',
+                                data: $scope.event,
+                                success: function(response){
+                                    console.log(response);
+                                }
+                            });
+                        }            
 
 		
         }
