8722ff3b64350a7214973b36bfb3f24bac6c7afd
agc123 <agc123@yeah.net>
2016-10-27 03:04:37 -0400

    dashboard ng-dialog

--- dashboard/dashboard.controller.js
+++ dashboard/dashboard.controller.js
@@ -5,8 +5,8 @@
         .module('mainApp')
         .controller('DashboardController', DashboardController);
 
-    DashboardController.$inject = ['$scope', '$location', 'FlashService'];
-        function DashboardController($scope, $location, FlashService) {
+    DashboardController.$inject = ['$scope', '$location', '$cookies','FlashService', 'ngDialog'];
+        function DashboardController($scope, $location, $cookies, FlashService, ngDialog) {
             
             $("#bodyBackground").css('background', 'white');
             $scope.allevents = [];
@@ -49,6 +49,57 @@
                   var date = a.getDate();
                   var time = month + ' ' + date + ' ' +  year;
                   return time;
+            }
+
+            $scope.showspecificevent = function(event) {
+                ngDialog.open({ 
+                    template: 'templateId',
+                    controller: ['$scope', '$cookies' , function($scope, $cookies) {
+                        $scope.userinfo = $cookies.getObject('globals') || {};
+                        $scope.show = true;
+                        $scope.reserve = true;
+                        //$scope.username = $scope.userinfo.currentUser.username;
+                        $scope.email = $scope.userinfo.currentUser.email;
+                        if ($scope.email == event.owner) {
+                            $scope.show = false;
+                        }
+                        $scope.timeConverter = function(UNIX_timestamp){
+                            var a = new Date(UNIX_timestamp);
+                            var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+                            var year = a.getFullYear();
+                            var month = months[a.getMonth()];
+                            var date = a.getDate();
+                            var hour = a.getHours();
+                            var minute = a.getMinutes();
+                            var time = month + ' ' + date + ' ' +  year + '   ' + hour + ':' + minute;
+                            return time;
+                        }
+                        event.starttime = $scope.timeConverter(event.time);
+                        event.endtime = $scope.timeConverter(event.time + event.duration);
+                        event.posttime = $scope.timeConverter(event.timeposted);
+                        $scope.specevent = event;
+                        console.log($scope.specevent);
+                        if (event.latitude) {
+                           $scope.mapurl="https://maps.googleapis.com/maps/api/staticmap?center=" + event.latitude + "," + event.longitude + 
+                                    "&zoom=16&size=320x200&&markers=color:red%7Clabel:C%7C" + event.latitude + "," + event.longitude 
+                                    + "&key=AIzaSyAFhzO5tGWXiCCtH5y6XW6ycS-1fbC4uYA";
+                        } else {
+                            $scope.mapurl="img/loc_404.png";
+                        }
+
+                        $scope.reserveEvent = function() {
+                            $scope.reserve = false;
+                        }
+
+                        $scope.quitEvent = function() {
+                            $scope.reserve = true;
+                        }
+
+
+                        
+                        
+                    }] 
+                });
             }
 
         };
