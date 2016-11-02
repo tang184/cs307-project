1e54ffabb262f5614d3ccbeefec95f01d73fccfd
agc123 <agc123@yeah.net>
2016-11-02 19:45:20 -0400

    upload event image

--- event/eventedit.js
+++ event/eventedit.js
@@ -108,6 +108,22 @@
                     window.history.back();
                 }
 
+                $("#file").change(function () {
+                    filePreview(this);
+                });
+
+                function filePreview(input) {
+                    if (input.files && input.files[0]) {
+                        var reader = new FileReader();
+                        reader.onload = function (e) {
+                            $('#uploadForm + img').remove();
+                            $('#uploadForm').after('<img src="'+e.target.result+'" width="450" height="300"/>');
+
+                        }
+                        reader.readAsDataURL(input.files[0]);
+                    }
+                }
+
 
         }
 
